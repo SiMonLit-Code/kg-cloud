479262045079c4348070edcc7ce2fc8932d81f52
wanglong <wanglong@plantdata.ai>
2019-12-12 17:33:58 +0800

    perf(all): 优化参数打印

--- kgdp/src/main/java/com/plantdata/kgcloud/KgdpApplication.java
+++ kgdp/src/main/java/com/plantdata/kgcloud/KgdpApplication.java
@@ -2,14 +2,12 @@ package com.plantdata.kgcloud;
 
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import com.plantdata.kgcloud.constant.CommonConstants;
-import com.plantdata.kgcloud.util.PropertyUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableDiscoveryClient
@@ -19,8 +17,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class KgdpApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(KgdpApplication.class, args);
-        PropertyUtils.printEnvAll(configurableApplicationContext);
+        SpringApplication.run(KgdpApplication.class, args);
     }
 
     @Override
