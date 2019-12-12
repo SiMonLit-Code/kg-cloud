79eeb5b69883954f6ded814e04d5b3ceba205f8e
lebovin <lebovin@qq.com>
2019-12-12 10:46:38 +0800

    feat(kgms): 菜单订阅

--- kgms/src/main/java/com/plantdata/kgcloud/constant/KgmsErrorCodeEnum.java
+++ kgms/src/main/java/com/plantdata/kgcloud/constant/KgmsErrorCodeEnum.java
@@ -24,6 +24,9 @@ public enum KgmsErrorCodeEnum implements ErrorCode {
     DATASET_EXPORT_FAIL(120107, "数据集导出失败"),
     DATASET_IMPORT_FAIL(120108, "数据集导出失败"),
     ANNOTATION_NOT_EXISTS(120109, "标引不存在"),
+    DATASET_ES_REQUEST_ERROR(120110,"es请求失败"),
+    DATASET_ES_KEY_EXISTS(120111,"es唯一标识已存在"),
+
 
     MODEL_NOT_EXISTS(120201, "模型不存在"),
     /**
--- kgms/src/main/java/com/plantdata/kgcloud/domain/menu/rsp/MenuFavorRsp.java
+++ kgms/src/main/java/com/plantdata/kgcloud/domain/menu/rsp/MenuFavorRsp.java
@@ -1,11 +1,19 @@
 package com.plantdata.kgcloud.domain.menu.rsp;
 
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
 /**
  * @description:
  * @author: Bovin
  * @create: 2019-11-04 18:45
  **/
+@Data
 public class MenuFavorRsp {
-
-
+    private String userId;
+    private List<Integer> menuId;
+    private Date createAt;
+    private Date updateAt;
 }
