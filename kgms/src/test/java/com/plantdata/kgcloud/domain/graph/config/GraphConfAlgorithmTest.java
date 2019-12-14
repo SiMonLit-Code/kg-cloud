5785465ddb9376550efbe37978a33fba23b98388
姜德明 <1@1>
2019-12-14 09:09:22 +0800

    feat(kgms-graph-kgql): 测试用例

--- kgms/src/test/java/com/plantdata/kgcloud/domain/graph/config/GraphConfAlgorithmTest.java
+++ kgms/src/test/java/com/plantdata/kgcloud/domain/graph/config/GraphConfAlgorithmTest.java
@@ -10,7 +10,7 @@ import com.plantdata.kgcloud.util.JacksonUtils;
 import com.plantdata.kgcloud.util.KgKeyGenerator;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.BeanUtils;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
