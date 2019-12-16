7a48410b79ed2c34d9d362de4103065adaf31802
wanglong <wanglong@plantdata.ai>
2019-12-16 10:22:31 +0800

    feat(kgdp): 移除kgdp模块

--- kgdp/src/main/java/com/plantdata/kgcloud/domain/common/req/FileReq.java
+++
@@ -1,9 +0,0 @@
-package com.plantdata.kgcloud.domain.common.req;
-
-import lombok.Data;
-
-@Data
-public class FileReq {
-
-    private String name;
-}
--- kgdp/src/main/java/com/plantdata/kgcloud/domain/document/repository/AttrRepository.java
+++
@@ -1,7 +0,0 @@
-package com.plantdata.kgcloud.domain.document.repository;
-
-import com.plantdata.kgcloud.domain.document.entity.Attr;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AttrRepository extends JpaRepository<Attr,Integer> {
-}
