e30e2aa53a14cdcfe764e5be67339dd17beb5392
lebovin <lebovin@qq.com>
2019-12-25 10:45:35 +0800

    fix(kgms): 图谱查询

--- kgms/src/main/java/com/plantdata/kgcloud/domain/graph/manage/repository/GraphRepository.java
+++ kgms/src/main/java/com/plantdata/kgcloud/domain/graph/manage/repository/GraphRepository.java
@@ -2,13 +2,8 @@ package com.plantdata.kgcloud.domain.graph.manage.repository;
 
 import com.plantdata.kgcloud.domain.graph.manage.entity.Graph;
 import com.plantdata.kgcloud.domain.graph.manage.entity.GraphPk;
-import org.apache.poi.ss.formula.functions.T;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 
 /**
@@ -16,17 +11,7 @@ import java.util.List;
  * @author: Bovin
  * @create: 2019-11-04 18:45
  **/
-public interface GraphRepository extends JpaRepository<Graph, GraphPk> {
-
-    /**
-     * 查询
-     *
-     * @param userId
-     * @param kw
-     * @param pageable
-     * @return
-     */
-    Page<Graph> findByUserIdAndTitleContaining(String userId, String kw, Pageable pageable);
+public interface GraphRepository extends JpaRepository<Graph, GraphPk>, JpaSpecificationExecutor<Graph> {
 
     Graph findByKgNameAndUserId(String kgName, String userId);
 }
