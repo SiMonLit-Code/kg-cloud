46633e206928fad6447f037236916d58b76e9959
lebovin <lebovin@qq.com>
2019-12-27 11:46:11 +0800

    fix(request): 请求参数校验

--- kgms-sdk/src/main/java/com/plantdata/kgcloud/sdk/req/DataSetCreateReq.java
+++ kgms-sdk/src/main/java/com/plantdata/kgcloud/sdk/req/DataSetCreateReq.java
@@ -3,8 +3,11 @@ package com.plantdata.kgcloud.sdk.req;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.util.List;
 
 /**
@@ -23,6 +26,9 @@ public class DataSetCreateReq {
     private String title;
 
     @ApiModelProperty("唯一标识")
+    @NotBlank(message = "唯一标识不能为空")
+    @Length(min = 4, max = 20, message = "唯一标识长度必须在4-20之间")
+    @Pattern(regexp = "^[a-z]\\w{4,19}$", message = "唯一标识必须以小写字母开头，可由字母数字下划线组成")
     private String key;
 
     @ApiModelProperty("数据集类型")
