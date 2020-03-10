f56ba73d51e9cd17045463f84f7a31a56ef89a16
chenjiawei <cjwfuture@163.com>
2020-03-10 18:34:04 +0800

    feat(doc): required

--- kgsdk/src/main/java/com/plantdata/kgcloud/domain/data/obtain/controller/EntityController.java
+++ kgsdk/src/main/java/com/plantdata/kgcloud/domain/data/obtain/controller/EntityController.java
@@ -33,21 +33,22 @@ public class EntityController implements GraphDataObtainInterface {
     @Autowired
     private EditClient editClient;
 
-    @ApiOperation(value = "批量实体查询",notes = "按数值属性筛选实例")
+    @ApiOperation(value = "批量实体查询", notes = "按数值属性筛选实例")
     @PostMapping("{kgName}/list")
     public ApiReturn<List<OpenEntityRsp>> queryEntityList(@PathVariable("kgName") String kgName,
-                                                         @RequestBody EntityQueryReq entityQueryReq) {
-        return editClient.queryEntityList(kgName,entityQueryReq);
+                                                          @RequestBody EntityQueryReq entityQueryReq) {
+        return editClient.queryEntityList(kgName, entityQueryReq);
     }
 
-    @ApiOperation(value = "批量实体新增及更新",notes = "新增实体节点及其数值属性，或更新实体的数值属性")
+    @ApiOperation(value = "批量实体新增及更新", notes = "新增实体节点及其数值属性，或更新实体的数值属性")
     @PostMapping("{kgName}")
-    public ApiReturn<OpenBatchResult<OpenBatchSaveEntityRsp>> batchAdd(@PathVariable("kgName") String kgName, @ApiParam("true修改") @RequestParam boolean add,
+    public ApiReturn<OpenBatchResult<OpenBatchSaveEntityRsp>> batchAdd(@PathVariable("kgName") String kgName,
+                                                                       @ApiParam(value = "true修改", required = true) @RequestParam boolean add,
                                                                        @RequestBody List<OpenBatchSaveEntityRsp> batchEntity) {
         return editClient.saveOrUpdate(kgName, add, batchEntity);
     }
 
-    @ApiOperation(value = "批量实体删除",notes = "删除实体。")
+    @ApiOperation(value = "批量实体删除", notes = "删除实体。")
     @PostMapping("{kgName}/batch/delete")
     public ApiReturn<List<DeleteResult>> batchDeleteEntities(@PathVariable("kgName") String kgName,
                                                              @RequestBody List<Long> ids) {
