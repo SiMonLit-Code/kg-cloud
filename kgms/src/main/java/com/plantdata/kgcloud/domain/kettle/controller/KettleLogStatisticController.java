bf8519f04e525a97d991c51291a94ae88fc3c2fd
chenjiawei <cjwfuture@163.com>
2020-04-02 19:13:18 +0800

    feat(kgms-dataStore): 数仓统计略修改v2

--- kgms/src/main/java/com/plantdata/kgcloud/domain/kettle/controller/KettleLogStatisticController.java
+++ kgms/src/main/java/com/plantdata/kgcloud/domain/kettle/controller/KettleLogStatisticController.java
@@ -1,13 +1,18 @@
 package com.plantdata.kgcloud.domain.kettle.controller;
 
+import com.google.common.collect.Lists;
 import com.plantdata.kgcloud.bean.ApiReturn;
+import com.plantdata.kgcloud.domain.app.util.JsonUtils;
 import com.plantdata.kgcloud.domain.dw.req.KettleLogStatisticReq;
+import com.plantdata.kgcloud.domain.dw.rsp.GraphMapRsp;
 import com.plantdata.kgcloud.domain.dw.rsp.KettleLogStatisticRsp;
 import com.plantdata.kgcloud.domain.kettle.service.KettleLogStatisticService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 /**
  * @author Administrator
@@ -26,6 +31,12 @@ public class KettleLogStatisticController {
     public ApiReturn<KettleLogStatisticRsp> kettleLogStatisticByDate(
             @ApiParam("数仓id") @PathVariable("id") long dataId,
             @RequestBody KettleLogStatisticReq statisticReq) {
+        GraphMapRsp graphMapRsp = new GraphMapRsp();
+        graphMapRsp.setDataBaseId(164L);
+        graphMapRsp.setTableName("paper");
+        ArrayList<GraphMapRsp> graphMapRsps = Lists.newArrayList(graphMapRsp);
+        kettleLogStatisticService.fillGraphMapRspCount(graphMapRsps);
+        System.out.println(JsonUtils.objToJson(graphMapRsps));
         return ApiReturn.success(kettleLogStatisticService.kettleLogStatisticByDate(dataId,statisticReq));
     }
 }
