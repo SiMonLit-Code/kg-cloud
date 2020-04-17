5baa28b3010a3cc5ce698111c14bee4d7e068f4d
qinwenhao <188813468@qq.com>
2020-04-17 10:43:36 +0800

    //数仓列表状态

--- kgms/src/main/java/com/plantdata/kgcloud/domain/data/entity/DWData.java
+++ kgms/src/main/java/com/plantdata/kgcloud/domain/data/entity/DWData.java
@@ -5,6 +5,8 @@ import io.swagger.annotations.ApiModel;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 /**
  * @Author: LinHo
  * @Date: 2020/3/24 15:04
@@ -19,5 +21,16 @@ public class DWData {
 
     private String tableName;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DWData dwData = (DWData) o;
+        return Objects.equals(tableName, dwData.tableName);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(tableName);
+    }
 }
