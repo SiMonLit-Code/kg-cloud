5db32e0b0afecbc3bd10bd8087d7e5f7a8f63ddc
qinwenhao <188813468@qq.com>
2020-04-22 16:42:13 +0800

    // 优化 数仓数据更新 并插入新表

--- kgms/src/main/java/com/plantdata/kgcloud/domain/dw/req/DWDatabaseUpdateReq.java
+++ kgms/src/main/java/com/plantdata/kgcloud/domain/dw/req/DWDatabaseUpdateReq.java
@@ -4,22 +4,21 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
 import java.util.Map;
 
 @Data
 @ApiModel("数仓表数据更改")
 public class DWDatabaseUpdateReq {
-
+    @NotBlank(message = "数仓id不能为空")
     @ApiModelProperty("数仓id")
     private Long dataBaseId;
-
+    @NotBlank(message = "表id不能为空")
     @ApiModelProperty("表id")
     private Long tableId;
+    @NotBlank(message = "数据id不能为空")
     @ApiModelProperty("数据id")
     private String id;
-
     @ApiModelProperty("数据")
     private Map<String, Object> data;
-
-
 }
