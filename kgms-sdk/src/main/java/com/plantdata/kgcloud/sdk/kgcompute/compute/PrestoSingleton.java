66e1e6f114321dff674c5cb1c0aca6136b775111
曹煦 <pototo@yeah.net>
2020-04-22 08:54:20 +0800

    数仓统计

--- kgms-sdk/src/main/java/com/plantdata/kgcloud/sdk/kgcompute/compute/PrestoSingleton.java
+++ kgms-sdk/src/main/java/com/plantdata/kgcloud/sdk/kgcompute/compute/PrestoSingleton.java
@@ -2,19 +2,25 @@ package com.plantdata.kgcloud.sdk.kgcompute.compute;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Set;
+
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.ConfigService;
 
 public class PrestoSingleton {
-
-    private static String presto_conn_info = "192.168.4.145:16666";
-
-    private static String jdbc_url = "jdbc:presto://"+presto_conn_info+"/system/runtime";
+    static Config appConfig;;
 
     private static Connection connection = null;
 
     private static void init(){
         try {
+            appConfig = ConfigService.getConfig("kgsdk");
+            String presto_conn_info = appConfig.getProperty("presto.server",null);
+            String jdbc_url = "jdbc:presto://"+presto_conn_info+"/system/runtime";
+            String userName = appConfig.getProperty("presto.userName",null);
+            String password = appConfig.getProperty("presto.password",null);
             Class.forName("com.facebook.presto.jdbc.PrestoDriver");
-            connection = DriverManager.getConnection(jdbc_url,"root","");
+            connection = DriverManager.getConnection(jdbc_url,userName,password);
         } catch (Exception e) {
             e.printStackTrace();
         }
--- kgms/src/main/java/com/plantdata/kgcloud/domain/app/controller/DataWarehouseStatisticController.java
+++ kgms/src/main/java/com/plantdata/kgcloud/domain/app/controller/DataWarehouseStatisticController.java
@@ -1,9 +1,17 @@
 package com.plantdata.kgcloud.domain.app.controller;
 
 import com.plantdata.kgcloud.bean.ApiReturn;
+import com.plantdata.kgcloud.bean.BasePage;
 import com.plantdata.kgcloud.domain.app.controller.module.SdkOpenApiInterface;
 import com.plantdata.kgcloud.domain.app.service.DataSetStatisticService;
 import com.plantdata.kgcloud.domain.app.service.DataWarehouseStatisticService;
+import com.plantdata.kgcloud.domain.data.req.DataStoreScreenReq;
+import com.plantdata.kgcloud.domain.data.req.DtReq;
+import com.plantdata.kgcloud.domain.data.rsp.DataStoreRsp;
+import com.plantdata.kgcloud.domain.data.rsp.DbAndTableRsp;
+import com.plantdata.kgcloud.domain.data.service.DataStoreService;
+import com.plantdata.kgcloud.domain.dw.rsp.DWDatabaseRsp;
+import com.plantdata.kgcloud.domain.dw.service.DWService;
 import com.plantdata.kgcloud.sdk.constant.DimensionEnum;
 import com.plantdata.kgcloud.sdk.req.SqlQueryReq;
 import com.plantdata.kgcloud.sdk.req.StatisticByDimensionalReq;
@@ -12,12 +20,14 @@ import com.plantdata.kgcloud.sdk.req.app.DataSetStatisticRsp;
 import com.plantdata.kgcloud.security.SessionHolder;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.plantdata.kgcloud.sdk.rsp.DataWarehouse2dTableRsp;
 import com.plantdata.kgcloud.sdk.rsp.DataWarehouse3dTableRsp;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.List;
 
 /**
  * @author cx
@@ -31,6 +41,12 @@ public class DataWarehouseStatisticController implements SdkOpenApiInterface {
     @Resource
     private DataWarehouseStatisticService dataWarehouseStatisticService;
 
+    @Autowired
+    DataStoreService service;
+
+    @Autowired
+    private DWService dwServince;
+
     @ApiOperation("统计数据二维/按表统计")
     @PostMapping("by2dTable")
     public ApiReturn<DataWarehouse2dTableRsp> statistic2dByTable(@Valid @RequestBody SqlQueryReq req) {
@@ -40,6 +56,8 @@ public class DataWarehouseStatisticController implements SdkOpenApiInterface {
     @ApiOperation("统计数据三维/按表统计")
     @PostMapping("by3dTable")
     public ApiReturn<DataWarehouse3dTableRsp> statistic3dByTable(@Valid @RequestBody SqlQueryReq req) {
+        String userId = SessionHolder.getUserId();
+        List<DWDatabaseRsp> a = dwServince.databaseTableList(userId);
         return ApiReturn.success(dataWarehouseStatisticService.statisticBy3DTable(req));
     }
 }
