c9aa357b58edd7e9405afa146e7f034ab15714cb
曹煦 <pototo@yeah.net>
2020-04-26 14:30:00 +0800

    修复保存图谱问答会删除所有现有图谱问答的bug

--- kgms/src/main/java/com/plantdata/kgcloud/domain/data/service/impl/DataStoreServiceImpl.java
+++ kgms/src/main/java/com/plantdata/kgcloud/domain/data/service/impl/DataStoreServiceImpl.java
@@ -1,6 +1,7 @@
 package com.plantdata.kgcloud.domain.data.service.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.mongodb.MongoClient;
