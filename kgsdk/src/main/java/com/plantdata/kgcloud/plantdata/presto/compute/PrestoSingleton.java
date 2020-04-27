e163e0ff82e4e8d1e3f3d8be57d5ece5aa518e47
曹煦 <pototo@yeah.net>
2020-04-27 20:41:53 +0800

    修改阿波罗配置

--- kgsdk/src/main/java/com/plantdata/kgcloud/plantdata/presto/compute/PrestoSingleton.java
+++ kgsdk/src/main/java/com/plantdata/kgcloud/plantdata/presto/compute/PrestoSingleton.java
@@ -15,9 +15,7 @@ public class PrestoSingleton {
     private static void init(){
         try {
             appConfig = ConfigService.getConfig("kgsdk");
-            String presto_conn_info = appConfig.getProperty("presto.server",null);
-            String path = appConfig.getProperty("presto.path",null);
-            String jdbc_url = "jdbc:presto://"+presto_conn_info+path;
+            String jdbc_url = appConfig.getProperty("presto.path",null);
             String userName = appConfig.getProperty("presto.user",null);
             String password = appConfig.getProperty("presto.password",null);
             Class.forName(appConfig.getProperty("presto.class-name",null));
