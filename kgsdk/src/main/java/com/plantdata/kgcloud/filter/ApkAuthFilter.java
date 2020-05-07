71eb356f3b3fc6b5abf5cbb24ef84b7a5886e3e1
lebovin <lebovin@qq.com>
2020-05-07 13:52:49 +0800

    feat(kgpreview): kgpreview添加 sso 支持

--- kgsdk/src/main/java/com/plantdata/kgcloud/filter/ApkAuthFilter.java
+++ kgsdk/src/main/java/com/plantdata/kgcloud/filter/ApkAuthFilter.java
@@ -8,6 +8,10 @@ import com.plantdata.kgcloud.sdk.rsp.LoginRsp;
 import com.plantdata.kgcloud.util.WebUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -27,31 +31,43 @@ public class ApkAuthFilter extends OncePerRequestFilter {
     private SsoClient ssoClient;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
-        String apk = WebUtils.getKgApk(httpServletRequest);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            if(authentication instanceof OAuth2Authentication) {
+                OAuth2Authentication oauth = (OAuth2Authentication) authentication;
+                OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) oauth.getDetails();
+                CurrentUser.setAdmin(false);
+                CurrentUser.setToken(details.getTokenType() + " " + details.getTokenValue());
+                filterChain.doFilter(request, response);
+                return;
+            }
+        }
+
+        String apk = WebUtils.getKgApk(request);
         if (!StringUtils.hasText(apk)) {
-            filterChain.doFilter(httpServletRequest, httpServletResponse);
+            filterChain.doFilter(request, response);
             return;
         }
         if(StringUtils.hasText(CurrentUser.getToken())){
-            filterChain.doFilter(httpServletRequest, httpServletResponse);
+            filterChain.doFilter(request, response);
             return;
         }
-        String authorization = WebUtils.getAuthorization(httpServletRequest);
+        String authorization = WebUtils.getAuthorization(request);
         if(StringUtils.hasText(authorization)){
-            filterChain.doFilter(httpServletRequest, httpServletResponse);
+            filterChain.doFilter(request, response);
             return;
         }
         //非管理员需要登录(兼容旧接口)
-        Optional<LoginRsp> loginOpt = login(apk, httpServletResponse);
+        Optional<LoginRsp> loginOpt = login(apk, response);
         if (!loginOpt.isPresent()) {
-            filterChain.doFilter(httpServletRequest, httpServletResponse);
+            filterChain.doFilter(request, response);
             return;
         }
         LoginRsp loginRsp = loginOpt.get();
         CurrentUser.setAdmin(loginRsp.isAdmin());
         CurrentUser.setToken(loginRsp.getToken());
-        filterChain.doFilter(httpServletRequest, httpServletResponse);
+        filterChain.doFilter(request, response);
     }
 
     private Optional<LoginRsp> login(String apk, HttpServletResponse httpServletResponse) throws IOException {
