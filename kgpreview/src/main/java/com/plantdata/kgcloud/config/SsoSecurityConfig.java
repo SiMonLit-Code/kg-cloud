1e04752235cc6029f9353775b0cde46e2a651cdf
lebovin <lebovin@qq.com>
2020-05-07 20:14:20 +0800

    fix(kgms,kgsdk): 健康检测

--- kgpreview/src/main/java/com/plantdata/kgcloud/config/SsoSecurityConfig.java
+++ kgpreview/src/main/java/com/plantdata/kgcloud/config/SsoSecurityConfig.java
@@ -4,7 +4,6 @@ import com.plantdata.kgcloud.util.WebUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
