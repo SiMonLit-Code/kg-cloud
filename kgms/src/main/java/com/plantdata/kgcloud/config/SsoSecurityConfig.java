43719509c160058429dbe34424013bc1ef74f463
lebovin <lebovin@qq.com>
2020-05-07 15:36:43 +0800

    fix(sso): 解决跨域问题

--- kgms/src/main/java/com/plantdata/kgcloud/config/SsoSecurityConfig.java
+++ kgms/src/main/java/com/plantdata/kgcloud/config/SsoSecurityConfig.java
@@ -3,12 +3,15 @@ package com.plantdata.kgcloud.config;
 import com.plantdata.kgcloud.util.WebUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
 @EnableResourceServer
+@Order(-1)
 public class SsoSecurityConfig extends ResourceServerConfigurerAdapter {
 
     @Value("${sso.allowedPaths:}")
@@ -18,6 +21,8 @@ public class SsoSecurityConfig extends ResourceServerConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http.cors();
         http.authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS)
+                .permitAll()
                 .antMatchers(WebUtils.DEFAULT_IGNORE)
                 .permitAll()
                 .anyRequest()
