f617866eafff6b62d404ac3087873c4069a3a178
lebovin <lebovin@qq.com>
2020-05-14 18:12:30 +0800

    fix(sso): 调整接口调用排除顺序

--- kgsdk/src/main/java/com/plantdata/kgcloud/config/SsoSecurityConfig.java
+++ kgsdk/src/main/java/com/plantdata/kgcloud/config/SsoSecurityConfig.java
@@ -1,11 +1,14 @@
 package com.plantdata.kgcloud.config;
 
+import com.plantdata.kgcloud.filter.ApkAuthFilter;
 import com.plantdata.kgcloud.util.WebUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 
 @Configuration
 @EnableResourceServer
@@ -14,9 +17,11 @@ public class SsoSecurityConfig extends ResourceServerConfigurerAdapter {
     @Value("${sso.allowedPaths:}")
     private String[] ignorePaths;
 
+    @Autowired
+    private ApkAuthFilter apkAuthFilter;
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.cors();
         http.authorizeRequests()
                 .antMatchers(WebUtils.DEFAULT_IGNORE)
                 .permitAll()
@@ -25,7 +30,7 @@ public class SsoSecurityConfig extends ResourceServerConfigurerAdapter {
         if (ignorePaths != null && ignorePaths.length > 0) {
             http.authorizeRequests().antMatchers(ignorePaths).permitAll();
         }
-
+        http.addFilterBefore(apkAuthFilter, AnonymousAuthenticationFilter.class);
     }
 
 }
--- kgsdk/src/main/java/com/plantdata/kgcloud/filter/ApkAuthFilter.java
+++ kgsdk/src/main/java/com/plantdata/kgcloud/filter/ApkAuthFilter.java
@@ -8,6 +8,14 @@ import com.plantdata.kgcloud.sdk.rsp.LoginRsp;
 import com.plantdata.kgcloud.util.WebUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -17,6 +25,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Optional;
 
 @Slf4j
@@ -26,32 +35,45 @@ public class ApkAuthFilter extends OncePerRequestFilter {
     @Autowired
     private SsoClient ssoClient;
 
+    @Autowired
+    private ApplicationEventPublisher eventPublisher;
+
     @Override
-    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
-        String apk = WebUtils.getKgApk(httpServletRequest);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            if(authentication instanceof OAuth2Authentication) {
+                OAuth2Authentication oauth = (OAuth2Authentication) authentication;
+                OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) oauth.getDetails();
+                CurrentUser.setAdmin(false);
+                CurrentUser.setToken(details.getTokenType() + " " + details.getTokenValue());
+                filterChain.doFilter(request, response);
+            return;
+        }
+        }
+
+        String apk = WebUtils.getKgApk(request);
         if (!StringUtils.hasText(apk)) {
-            filterChain.doFilter(httpServletRequest, httpServletResponse);
+            filterChain.doFilter(request, response);
             return;
         }
-        if(StringUtils.hasText(CurrentUser.getToken())){
-            filterChain.doFilter(httpServletRequest, httpServletResponse);
-            return;
-        }
-        String authorization = WebUtils.getAuthorization(httpServletRequest);
-        if(StringUtils.hasText(authorization)){
-            filterChain.doFilter(httpServletRequest, httpServletResponse);
-            return;
-        }
+
         //非管理员需要登录(兼容旧接口)
-        Optional<LoginRsp> loginOpt = login(apk, httpServletResponse);
+        Optional<LoginRsp> loginOpt = login(apk, response);
         if (!loginOpt.isPresent()) {
-            filterChain.doFilter(httpServletRequest, httpServletResponse);
+            filterChain.doFilter(request, response);
             return;
         }
         LoginRsp loginRsp = loginOpt.get();
         CurrentUser.setAdmin(loginRsp.isAdmin());
         CurrentUser.setToken(loginRsp.getToken());
-        filterChain.doFilter(httpServletRequest, httpServletResponse);
+
+
+        UsernamePasswordAuthenticationToken result =
+                new UsernamePasswordAuthenticationToken("a","b", Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
+        eventPublisher.publishEvent(new AuthenticationSuccessEvent(result));
+        SecurityContextHolder.getContext().setAuthentication(result);
+        filterChain.doFilter(request, response);
     }
 
     private Optional<LoginRsp> login(String apk, HttpServletResponse httpServletResponse) throws IOException {
@@ -59,6 +81,7 @@ public class ApkAuthFilter extends OncePerRequestFilter {
         try {
             loginRspApiReturn = this.ssoClient.loginByApk(apk);
         } catch (Exception e) {
+            log.error(" ",e);
             WebUtils.sendResponse(httpServletResponse, ApiReturn.fail(CommonErrorCode.INTERNAL_SERVER_ERROR));
             return Optional.empty();
         }
@@ -67,6 +90,7 @@ public class ApkAuthFilter extends OncePerRequestFilter {
             return Optional.empty();
         }
         if (CommonErrorCode.SUCCESS.getErrorCode() != loginRspApiReturn.getErrCode()) {
+            log.error(" ",loginRspApiReturn);
             WebUtils.sendResponse(httpServletResponse, loginRspApiReturn);
             return Optional.empty();
         }
