c9a72fbd3a82c953d0cb16e4aa086389f00357a4
lebovin <lebovin@qq.com>
2020-05-23 15:04:55 +0800

    feat(repo): 组件

--- kgms/src/main/java/com/plantdata/kgcloud/domain/repo/entity/RepoBaseMenu.java
+++ kgms/src/main/java/com/plantdata/kgcloud/domain/repo/entity/RepoBaseMenu.java
@@ -1,6 +1,7 @@
 package com.plantdata.kgcloud.domain.repo.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
@@ -16,11 +17,14 @@ import javax.persistence.Table;
 @Getter
 @Setter
 @Entity
-@Table(name = "repo_menu")
+@Table(name = "repo_base_menu")
+@NoArgsConstructor
 public class RepoBaseMenu {
     @Id
     @Column(name = "menu_id")
     private Integer menuId;
+
     @Column(name = "repository_id")
     private Integer repositoryId;
+
 }
