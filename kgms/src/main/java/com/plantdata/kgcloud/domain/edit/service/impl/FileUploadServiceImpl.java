4984492bcdf07e78ec8f9027eb589038c670f0ec
EYE <1137580313@qq.com>
2020-05-29 20:39:38 +0800

    修复文件上传bug

--- kgms/src/main/java/com/plantdata/kgcloud/domain/edit/service/impl/FileUploadServiceImpl.java
+++ kgms/src/main/java/com/plantdata/kgcloud/domain/edit/service/impl/FileUploadServiceImpl.java
@@ -1,9 +1,11 @@
 package com.plantdata.kgcloud.domain.edit.service.impl;
 
+import com.plantdata.kgcloud.constant.KgmsErrorCodeEnum;
 import com.plantdata.kgcloud.domain.edit.rsp.FilePathRsp;
 import com.plantdata.kgcloud.domain.edit.rsp.ThumbPathRsp;
 import com.plantdata.kgcloud.domain.edit.service.FileUploadService;
 import com.plantdata.kgcloud.dto.FastdfsPathDto;
+import com.plantdata.kgcloud.exception.BizException;
 import com.plantdata.kgcloud.template.FastdfsTemplate;
 import com.plantdata.kgcloud.util.ConvertUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @Author: LinHo
@@ -31,6 +35,12 @@ public class FileUploadServiceImpl implements FileUploadService {
 
     @Override
     public FilePathRsp uploadFile(MultipartFile file) {
+        String filename = file.getOriginalFilename();
+        String suffix = filename.substring(filename.lastIndexOf(".") + 1);
+        // 无后缀且文件名为中文 || 有后缀且后缀含有中文
+        if ((filename.equals(suffix) && isChineseChar(filename)) || (!filename.equals(suffix) && isChineseChar(suffix))) {
+            throw BizException.of(KgmsErrorCodeEnum.UPLOAD_FILENAME_NOT_TRUE);
+        }
         FastdfsPathDto fastdfsPathDto = fastdfsTemplate.uploadFile(file);
         return ConvertUtils.convert(FilePathRsp.class).apply(fastdfsPathDto);
     }
@@ -53,6 +63,11 @@ public class FileUploadServiceImpl implements FileUploadService {
             if (imageType.contains(fileExtName.toUpperCase())) {
                 fastdfsPathDto = fastdfsTemplate.uploadImage(file);
             } else {
+                // 无后缀且文件名为中文 || 有后缀且后缀含有中文
+                if ((originalFilename.equals(fileExtName) && isChineseChar(originalFilename))
+                        || (!originalFilename.equals(fileExtName) && isChineseChar(fileExtName))) {
+                    throw BizException.of(KgmsErrorCodeEnum.UPLOAD_FILENAME_NOT_TRUE);
+                }
                 fastdfsPathDto = fastdfsTemplate.uploadFile(file);
             }
             ThumbPathRsp thumbPathRsp = ConvertUtils.convert(ThumbPathRsp.class).apply(fastdfsPathDto);
@@ -64,6 +79,12 @@ public class FileUploadServiceImpl implements FileUploadService {
 
     @Override
     public ThumbPathRsp uploadPicture(MultipartFile file) {
+        String filename = file.getOriginalFilename();
+        String suffix = filename.substring(filename.lastIndexOf(".") + 1);
+        // 无后缀且文件名为中文 || 有后缀且后缀含有中文
+        if ((filename.equals(suffix) && isChineseChar(filename)) || (!filename.equals(suffix) && isChineseChar(suffix))) {
+            throw BizException.of(KgmsErrorCodeEnum.UPLOAD_FILENAME_NOT_TRUE);
+        }
         FastdfsPathDto fastdfsPathDto = fastdfsTemplate.uploadImage(file);
         return ConvertUtils.convert(ThumbPathRsp.class).apply(fastdfsPathDto);
     }
@@ -83,5 +104,21 @@ public class FileUploadServiceImpl implements FileUploadService {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 判断是否为汉字
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isChineseChar(String str) {
+        boolean temp = false;
+        Pattern p = Pattern.compile("[\u4e00-\u9fa5]");
+        Matcher m = p.matcher(str);
+        if (m.find()) {
+            temp = true;
+        }
+        return temp;
     }
 }
