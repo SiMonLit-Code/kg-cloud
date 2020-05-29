4c88ad3ec3c267ae67eba6c853bf6f26d0aeb9f9
EYE <1137580313@qq.com>
2020-05-29 14:34:23 +0800

    图谱备份去除原图谱参数

--- kgms/src/main/java/com/plantdata/kgcloud/domain/task/req/TaskGraphSnapshotNameReq.java
+++ kgms/src/main/java/com/plantdata/kgcloud/domain/task/req/TaskGraphSnapshotNameReq.java
@@ -17,10 +17,6 @@ public class TaskGraphSnapshotNameReq {
     private String name;
 
     @NotNull
-    @ApiModelProperty("备份原图谱名")
-    private String kgName;
-
-    @NotNull
     @ApiModelProperty("备份文件路径")
     private String fileName;
 
