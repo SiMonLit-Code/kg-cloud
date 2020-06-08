683c7e0449ce7d0ea91d9931ac2d96b15d495794
chenzj <chenzj@hiekn.com>
2020-06-08 18:35:28 +0800

    统计类业务算法

---
+++ kgsdk/src/main/java/com/plantdata/kgcloud/plantdata/rsp/app/statistic/AlgorithmStatisticeSeriesItemBean.java
@@ -0,0 +1,34 @@
+package com.plantdata.kgcloud.plantdata.rsp.app.statistic;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * @program: kg-cloud-kgms
+ * @description:
+ * @author: czj
+ * @create: 2020-06-07 15:16
+ **/
+@Getter
+@Setter
+@ApiModel("业务算法统计Series结构")
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlgorithmStatisticeSeriesItemBean {
+
+    private String name;
+
+    private List<Object> data;
+
+    @ApiModelProperty("data中统计的实体、关系id")
+    private List<List<Object>> ids;
+
+    @ApiModelProperty("表名ids中id是实体id还是关系id 1 实体 2关系")
+    private Integer type;
+}
