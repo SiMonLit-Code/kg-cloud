c70a1be29db4767b9e128a1bc95d5343843fb81c
chenzj <chenzj@hiekn.com>
2020-06-10 16:01:52 +0800

    模式管理区分图谱模式与数仓模式

---
+++ kgms/src/main/java/com/plantdata/kgcloud/domain/prebuilder/req/PreBuilderDeleteReq.java
@@ -0,0 +1,22 @@
+package com.plantdata.kgcloud.domain.prebuilder.req;
+
+import com.plantdata.kgcloud.domain.prebuilder.aop.BaseHandlerReq;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @program: kg-cloud-kgms
+ * @description:
+ * @author: czj
+ * @create: 2020-06-10 15:50
+ **/
+@Data
+public class PreBuilderDeleteReq  extends BaseHandlerReq {
+
+
+    @ApiModelProperty("模式id")
+    private Integer modelId;
+
+    @ApiModelProperty("模式类型 kg(图谱) dw(数仓)")
+    private String schemaType;
+}
