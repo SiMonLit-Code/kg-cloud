a714bb5f0275684b146a77de597a903b769dbd81
chenjiawei <cjwfuture@163.com>
2020-07-08 10:24:19 +0800

    新版sdk接口补充

--- kgsdk/src/main/java/com/plantdata/kgcloud/domain/data/obtain/controller/EntityController.java
+++ kgsdk/src/main/java/com/plantdata/kgcloud/domain/data/obtain/controller/EntityController.java
@@ -1,11 +1,14 @@
 package com.plantdata.kgcloud.domain.data.obtain.controller;
 
 import com.plantdata.kgcloud.bean.ApiReturn;
+import com.plantdata.kgcloud.bean.BasePage;
 import com.plantdata.kgcloud.domain.common.module.GraphDataObtainInterface;
 import com.plantdata.kgcloud.sdk.EditClient;
+import com.plantdata.kgcloud.sdk.KgDataClient;
 import com.plantdata.kgcloud.sdk.MergeClient;
 import com.plantdata.kgcloud.sdk.req.app.EntityQueryReq;
 import com.plantdata.kgcloud.sdk.req.app.OpenEntityRsp;
+import com.plantdata.kgcloud.sdk.req.app.TraceabilityQueryReq;
 import com.plantdata.kgcloud.sdk.rsp.OpenBatchResult;
 import com.plantdata.kgcloud.sdk.rsp.app.OpenBatchSaveEntityRsp;
 import com.plantdata.kgcloud.sdk.rsp.edit.DeleteResult;
@@ -35,6 +38,8 @@ public class EntityController implements GraphDataObtainInterface {
     private EditClient editClient;
     @Autowired
     private MergeClient mergeClient;
+    @Autowired
+    private KgDataClient kgDataClient;
 
     @ApiOperation(value = "批量实体查询", notes = "按数值属性筛选实例")
     @PostMapping("{kgName}/list")
@@ -62,5 +67,13 @@ public class EntityController implements GraphDataObtainInterface {
     @PostMapping("wait/create/{kgName}")
     public ApiReturn<String> createMergeEntity(@PathVariable("kgName") String kgName, @RequestBody List<Long> ids) {
         return mergeClient.createMergeEntity(kgName, ids);
+    }
+
+
+    @ApiOperation(value = "实体溯源", notes = "根据溯源信息查询实体的基本信息")
+    @PostMapping({"{kgName}/source/"})
+    public ApiReturn<BasePage<OpenEntityRsp>> queryEntityBySource(@PathVariable("kgName") String kgName,
+                                                                  @RequestBody TraceabilityQueryReq req) {
+        return kgDataClient.queryEntityBySource(kgName, req);
     }
 }
--- kgsdk/src/main/java/com/plantdata/kgcloud/domain/data/obtain/controller/QueryController.java
+++ kgsdk/src/main/java/com/plantdata/kgcloud/domain/data/obtain/controller/QueryController.java
@@ -3,9 +3,16 @@ package com.plantdata.kgcloud.domain.data.obtain.controller;
 import com.plantdata.kgcloud.bean.ApiReturn;
 import com.plantdata.kgcloud.domain.common.module.GraphDataObtainInterface;
 import com.plantdata.kgcloud.sdk.EditClient;
+import com.plantdata.kgcloud.sdk.KgDataClient;
+import com.plantdata.kgcloud.sdk.SemanticClient;
+import com.plantdata.kgcloud.sdk.req.app.SparQlReq;
+import com.plantdata.kgcloud.sdk.req.app.sematic.GremlinReq;
 import com.plantdata.kgcloud.sdk.req.edit.KgqlReq;
+import com.plantdata.kgcloud.sdk.rsp.app.GremlinRsp;
+import com.plantdata.kgcloud.sdk.rsp.app.sparql.QueryResultRsp;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,10 +30,26 @@ public class QueryController implements GraphDataObtainInterface {
 
     @Autowired
     private EditClient editClient;
+    @Autowired
+    private SemanticClient semanticClient;
+    @Autowired
+    private KgDataClient kgDataClient;
 
-    @ApiOperation(value = "kgql查询",notes = "KGQL查询，使用PlantData知识图谱的查询语言查询数据。")
+    @ApiOperation(value = "kgql查询", notes = "KGQL查询，使用PlantData知识图谱的查询语言查询数据。")
     @PostMapping("kgql")
     public ApiReturn executeQl(@Valid @RequestBody KgqlReq kgqlReq) {
         return editClient.executeQl(kgqlReq);
+    }
+
+    @ApiOperation(value = "gremlin查询", notes = "使用gremlin图查询语言查询数据。")
+    @PostMapping("gremlin/{kgName}")
+    public ApiReturn<GremlinRsp> gremlinQuery(@PathVariable("kgName") String kgName, @RequestBody GremlinReq req) {
+        return semanticClient.gremlinQuery(kgName, req);
+    }
+
+    @ApiOperation(value = "sparQl查询", notes = "使用sparQl查询语言查询数据。")
+    @PostMapping("sparQl/{kgName}")
+    public ApiReturn<QueryResultRsp> sparQlQuery(@PathVariable("kgName") String kgName, @RequestBody SparQlReq sparQlReq) {
+        return kgDataClient.sparQlQuery(kgName, sparQlReq);
     }
 }
