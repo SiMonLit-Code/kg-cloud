256ff1d2aaa2b43a31954bc5138460c3f7ba806a
lebovin <lebovin@qq.com>
2020-07-22 16:35:54 +0800

    fix(edit): 长度限制

--- kgms/src/main/java/ai/plantdata/kgcloud/domain/edit/req/entity/UpdateRelationMetaReq.java
+++ kgms/src/main/java/ai/plantdata/kgcloud/domain/edit/req/entity/UpdateRelationMetaReq.java
@@ -5,6 +5,9 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotEmpty;
 import java.util.HashMap;
 import java.util.Map;
@@ -23,6 +26,9 @@ public class UpdateRelationMetaReq {
     private String tripleId;
 
     @ApiModelProperty(value = "关系权重")
+    @DecimalMax(value = "1.0")
+    @DecimalMin(value = "0.0")
+    @Digits(integer = 1,fraction = 2,message = "只保留两位小数")
     private String score;
 
     @ApiModelProperty(value = "关系来源")
@@ -32,6 +38,9 @@ public class UpdateRelationMetaReq {
     private String sourceReason;
 
     @ApiModelProperty(value = "关系置信度")
+    @DecimalMax(value = "1.0")
+    @DecimalMin(value = "0.0")
+    @Digits(integer = 1,fraction = 2,message = "只保留两位小数")
     private String reliability;
 
     @ApiModelProperty(value = "关系开始时间")
